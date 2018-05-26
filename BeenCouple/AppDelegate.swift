a168429bc4894d517e8c5e08971c0ee491212603
Camelia <camelia0858@gmail.com>
2018-05-26 15:38:04 +0900

    Initial Commit

---
+++ BeenCouple/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  BeenCouple
+//
+//  Created by Camelia Park on 2018. 5. 26..
+//  Copyright © 2018년 Camelia Park. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
